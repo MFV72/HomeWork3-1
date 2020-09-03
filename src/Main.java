cd6fa9fccef0ccdfe3038d3106bbb03fc6015db9
mfv <mit.fedor@gmail.com>
2020-09-03 22:26:20 +0500

    add miles caclulation

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,11 @@
 public class Main {
     public static void main(String[] args) {
-        
+        int ticketPrice;
+        int count;
+        int totalMiles;
+        count = 20;
+        ticketPrice = 3333355; //стоимость билета в копейках
+        totalMiles = (ticketPrice / 100) / count;
+        System.out.println(totalMiles);
     }
 }
